a872af8ab46facb5fb0fb052abd997128314c9e5
Rekkisomo <im@rek.lol>
2026-06-13 01:01:39 +1000

    why are different characters different sizes

--- src/chunithm-net/profile.ts
+++ src/chunithm-net/profile.ts
@@ -36,7 +36,7 @@ function parseRatingImages(srcs: string[]): string {
 
 function absoluteUrl(src: string | undefined | null): string | null {
     if (!src) return null
-    return src.replace('chunithm-net-eng.com/', 'empty-brook-f048.rekkisomo.workers.dev/376/251/')
+    return src.replace('chunithm-net-eng.com/', 'empty-brook-f048.rekkisomo.workers.dev/360/240/')
 }
 
 export function parseProfile(html: string): PlayerProfile {
