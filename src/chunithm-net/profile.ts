c67f89f836e768c6240f018dee9a1e9b7b522135
Rekkisomo <im@rek.lol>
2026-06-13 00:03:49 +1000

    as if that would work

--- src/chunithm-net/profile.ts
+++ src/chunithm-net/profile.ts
@@ -36,7 +36,7 @@ function parseRatingImages(srcs: string[]): string {
 
 function absoluteUrl(src: string | undefined | null): string | null {
     if (!src) return null
-    return src.startsWith('http') ? src : `https://empty-brook-f048.rekkisomo.workers.dev${src}`
+    return src.startsWith('http') ? src.replace('chunithm-net-eng.com', 'empty-brook-f048.rekkisomo.workers.dev') : `https://empty-brook-f048.rekkisomo.workers.dev${src}`
 }
 
 export function parseProfile(html: string): PlayerProfile {
